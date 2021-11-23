032de6a56cf8e30df8987daeef9c4fbb14ab9ff5
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-11-23 17:49:35 -0600

    added all endpoints

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import main
+from .views import CreateRoomView, GetRoom, JoinRoom, LeaveRoom, RoomView, UpdateRoom, UserInRoom
 
 urlpatterns = [
-    path('', main)
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view())
+    
 ]
